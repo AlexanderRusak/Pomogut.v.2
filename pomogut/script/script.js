dc8a8367f5e93a02351af6b7815f710654353c5d
Alexander Rusak <375298388602@mail.ru>
2019-04-16 14:00:52 +0300

    making widgets page v.16 16.04.2019

--- pomogut/script/script.js
+++ pomogut/script/script.js
@@ -1,5 +1,45 @@
 $(document).on('ready', function() {
 
+  $('.about-logos').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 5,
+    infinite: false,
+    slidesToScroll: 1,
+    arrows: true,
+
+    responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: false,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+        }
+      },
+    ]
+  });
 jQuery(function($) {
   function fix_size() {
     var images = $('header .row .header-image img');
@@ -55,5 +95,4 @@ jQuery(function($) {
   fix_size();
 });
 
-});
 });
