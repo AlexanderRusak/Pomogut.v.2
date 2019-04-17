4072b714a2b47096a60a06cea6867cf5d1416b72
Alexander Rusak <375298388602@mail.ru>
2019-04-17 14:40:22 +0300

    making pomogut page v.16.5 17.04.2019

--- widgets-pages/slider/script/script.js
+++ widgets-pages/slider/script/script.js
@@ -2,6 +2,46 @@ $(document).on('ready', function() {
   $('.single-item').slick({
     dots: true,
     infinite: false,
+    responsive: [  {
+        breakpoint: 768,
+        settings: {
+          dots:false
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          dots:false
+        }
+      },
+    ]
+  });
+
+  $('.tags').slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerMode: false,
+    variableWidth: true,
+    responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+        }
+      }
+    ]
   });
 
 
