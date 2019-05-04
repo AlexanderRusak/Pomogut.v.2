9cb14c0b0fe80ab9b302656ba7eec4edd5f4c5a4
Alexander Rusak <375298388602@mail.ru>
2019-05-05 00:59:24 +0300

    made pages index  v.23 05.05.2019

--- index/script/script.js
+++ index/script/script.js
@@ -8,7 +8,7 @@ $(document).on('ready', function() {
       function setsize() {
         var img = $(this),
           img_dom = img.get(0),
-          container = img.parents('.block-1 .col .block-1-img img');
+          container = img.parents('.block-1 .col .block-1-img ');
         if (img_dom.complete) {
           resize();
         } else img.one('load', resize);
@@ -54,6 +54,32 @@ $(document).on('ready', function() {
     $(window).on('resize', fix_size);
     fix_size();
   });
+  jQuery(function($) {
+    function fix_size() {
+      var images = $('.block-2 .container .block-2-img  r img');
+      images.each(setsize);
 
+      function setsize() {
+        var img = $(this),
+          img_dom = img.get(0),
+          container = img.parents('.block-2 .container .block-2-img');
+        if (img_dom.complete) {
+          resize();
+        } else img.one('load', resize);
+
+        function resize() {
+          if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+            img.width('100%');
+            img.height('auto');
+            return;
+          }
+          img.height('100%');
+          img.width('auto');
+        }
+      }
+    }
+    $(window).on('resize', fix_size);
+    fix_size();
+  });
 
 });
