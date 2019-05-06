f36c88884e8eb15bf3e673c7ac05da8732d57643
Alexander Rusak <375298388602@mail.ru>
2019-05-06 03:47:45 +0300

    made pages index  v.23 05.05.2019

--- widgets-pages/header-footer/script/script.js
+++ widgets-pages/header-footer/script/script.js
@@ -54,6 +54,32 @@ $(document).on('ready', function() {
     $(window).on('resize', fix_size);
     fix_size();
   });
+  jQuery(function($) {
+    function fix_size() {
+      var images = $('.header .row div:nth-child(1) .logo-for-fixed img');
+      images.each(setsize);
 
+      function setsize() {
+        var img = $(this),
+          img_dom = img.get(0),
+          container = img.parents('.header .row div:nth-child(1) .logo-for-fixed)');
+        if (img_dom.complete) {
+          resize();
+        } else img.one('load', resize);
+
+        function resize() {
+          if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+            img.width('100%');
+            img.height('auto');
+            return;
+          }
+          img.height('100%');
+          img.width('auto');
+        }
+      }
+    }
+    $(window).on('resize', fix_size);
+    fix_size();
+  });
 
 });
