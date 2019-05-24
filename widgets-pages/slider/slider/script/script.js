c9104a7a6b07eeee04d7c7f4473c15695da5ea64
Alexander Rusak <375298388602@mail.ru>
2019-05-25 00:08:49 +0300

    start correct pages after Tanya's correction i'm correct header-footer index  v.25 14.05.2019

--- widgets-pages/slider/slider/script/script.js
+++ widgets-pages/slider/slider/script/script.js
@@ -6,15 +6,11 @@ $(document).on('ready', function() {
     responsive: [  {
         breakpoint: 768,
         variableWidth: true,
-        settings: {
-          dots:false
-        }
+        
       }, {
         breakpoint: 480,
         variableWidth: true,
-        settings: {
-          dots:false
-        }
+
       },
     ]
   });
