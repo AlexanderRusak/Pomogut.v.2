b2dede141bd590f4b4248ba65f666dc749cc53b0
Alexander Rusak <375298388602@mail.ru>
2019-06-10 23:49:08 +0300

    11.06.2019

--- pages/pages/script/script.js
+++ pages/pages/script/script.js
@@ -202,10 +202,11 @@ $(document).on('ready', function() {
   });
 
   $('.tags').slick({
+
     dots: false,
     arrows: true,
     infinite: false,
-    speed: 300,
+    speed: 1500,
     slidesToShow: 3,
     slidesToScroll: 3,
     centerMode: false,
